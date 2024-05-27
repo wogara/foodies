364b6c3178da679ea050520d2f3f4c690e1d5220
wogara <weogara@gmail.com>
2024-05-27 15:31:40 -0400

    added initial routing

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDetailsPage({ params }) {
+  return <h1>Meal Details</h1>;
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Meals Share</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
+
+      <p>
+        <Link href="/meals/meal-1">Meals meal 1 slug</Link>
+      </p>
     </main>
   );
 }
