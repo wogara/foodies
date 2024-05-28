5ffe878ac527c76f44b986c7629755f0f9cf6f6e
wogara <weogara@gmail.com>
2024-05-28 11:28:35 -0400

    styling updates, adding css modules and separating header

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import NavLink from "./nav-link";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
@@ -18,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
-            </li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+                 </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">FoodiesCommunity</NavLink>
             </li>
           </ul>
         </nav>
